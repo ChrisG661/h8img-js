352cbd083d20f591f38a6b1c2d14f8e09aa2b44c
Christopher G <chrismark0106@gmail.com>
2021-02-17 11:46:37 +0000

    idk what changed

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 // An overengineered yet buggy piece of ~art~ code
 
 console.log("hi :)");
-const BASE = "https://himawari8-dl.nict.go.jp/himawari8/img/D531106";
-const BASE_CORS = "himawari8-dl.nict.go.jp:443/himawari8/img/D531106";
-const CORS = "https://cors-px.chrisg661.repl.co/"; // CORS proxy
-const SCALE = 550;
+var BASE_URL = "https://himawari8-dl.nict.go.jp/himawari8/img/D531106";
+var BASE_CORS = "himawari8-dl.nict.go.jp:443/himawari8/img/D531106";
+var CORS = "https://cors-px.chrisg661.repl.co/"; // CORS proxy
+var SCALE = 550;
 var LEVEL = 2;
 var CURRENT_DATE;
 var canvas = document.getElementById("h8");
@@ -18,9 +18,9 @@ ctx.imageSmoothingQuality = "high";
 function altlatestdate() {
   console.log("Using alternative date method");
   var d = new Date();
-  d.setMinutes(Math.round((d.getMinutes() - 20)/10)*10);
+  d.setMinutes(Math.round((d.getMinutes() - 20) / 10) * 10);
   var year = d.getUTCFullYear();
-  var month = ("0" + (d.getUTCMonth()+1)).slice(-2);
+  var month = ("0" + (d.getUTCMonth() + 1)).slice(-2);
   var date = ("0" + d.getUTCDate()).slice(-2);
   var hour = ("0" + d.getUTCHours()).slice(-2);
   var minute = ("0" + d.getUTCMinutes()).slice(-2);
@@ -30,8 +30,7 @@ function altlatestdate() {
 
 function latestdate(level) {
   var uri = CORS + BASE_CORS + "/latest.json";
-  var date = (async function ()
-  {
+  var date = (async function () {
     var resp = await fetch(uri);
     if (resp.status != 200) {
       console.log("NOT OK!");
@@ -46,18 +45,18 @@ function latestdate(level) {
 }
 
 function latestUrl(x, y, level, date) {
-  var url = `${BASE}/${level}d/${SCALE}/${date}_${x}_${y}.png`;
+  var url = `${BASE_URL}/${level}d/${SCALE}/${date}_${x}_${y}.png`;
   return url;
 }
 
-async function getEarth(currentLevel, detailChange = false){
+async function getEarth(currentLevel, detailChange = false) {
   canvas.width = currentLevel * SCALE;
   canvas.height = currentLevel * SCALE;
   var h = [...Array(currentLevel).keys()];
   var v = [...Array(currentLevel).keys()];
   var img;
   var url;
-  var date = await latestdate(currentLevel).then(function (date) {return date});
+  var date = await latestdate(currentLevel).then(function (date) { return date });
   if (date == CURRENT_DATE && !detailChange) {
     return;
   }
@@ -71,12 +70,12 @@ async function getEarth(currentLevel, detailChange = false){
       //console.log(currentScale)
       img = new Image();
       img.onload = (
-        function(x, y, scale){
-          return function() {
-            ctx.drawImage(this, (x*scale), (y*scale), (scale), (scale));
+        function (x, y, scale) {
+          return function () {
+            ctx.drawImage(this, (x * scale), (y * scale), (scale), (scale));
           }
         }
-      )(x,y,currentScale);
+      )(x, y, currentScale);
       img.src = url;
     }
   }
@@ -96,8 +95,8 @@ function moreDetail() {
   getEarth(LEVEL, true);
 }
 
-var timer = setInterval(function() {
-    getEarth(LEVEL);
+var timer = setInterval(function () {
+  getEarth(LEVEL);
 }, 60 * 5 * 1000);
 
 canvas.onload = getEarth(LEVEL, true);
@@ -118,7 +117,7 @@ if (zoomcanvas) {
   // 2,4,8,16,20
   zoomcanvas.addEventListener("panzoomchange", (event) => {
     var prevLevel = LEVEL;
-    console.log(event.detail) // => { x: 0, y: 0, scale: 1 }
+    //console.log(event.detail) // => { x: 0, y: 0, scale: 1 }
     if (event.detail.scale <= 1.5) {
       LEVEL = 2
     }
@@ -138,11 +137,10 @@ if (zoomcanvas) {
       LEVEL = 20
     }
     else {
-      console.log("huh")
       LEVEL = 2
     }
     if (event.detail.scale == 1 && event.detail.x != 0 && event.detail.y != 0) {
-      panzoom.pan(0,0,{ force: true }) // reset
+      panzoom.pan(0, 0, { force: true }) // reset
     }
     if (LEVEL != prevLevel) {
       //console.log("current scale: ", prevLevel, LEVEL, event.detail.scale)
